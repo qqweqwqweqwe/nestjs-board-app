1cbf26de1d9be248cca6433750e834645ccf30fc
k0789789 <k0789789@naver.com>
2023-10-25 00:33:47 +0900

    Create board.model.ts

---
+++ src/boards/board.model.ts
@@ -0,0 +1,13 @@
+export interface Board{
+  id :string
+  title : string
+  description : string
+  status : BoardStatus // 공개글인지 비공개글인지
+
+}
+
+export enum BoardStatus{
+  PUBLIC = 'PUBLIC',
+  PRIVATE = 'PRIVATE'
+
+}
