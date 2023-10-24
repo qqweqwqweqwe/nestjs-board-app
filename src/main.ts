995c9979d55ea3f3175bab0aebe5c04563e0692f
k0789789 <k0789789@naver.com>
2023-10-25 00:33:10 +0900

    Feat 설명 관련 주석 추가

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // main.ts에서 AppModule을 생성해줌
+  // 그럼 AppModule에 등록된 컨트롤러랑 서비스를 가져옴
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
