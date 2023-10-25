63beb745e005cb22bb957a6287e92ea58cbb71f7
k0789789 <k0789789@naver.com>
2023-10-25 19:37:16 +0900

    Feat 유효성 검사 데코레이션 추가

--- src/boards/dto/create-board.dto.ts
+++ src/boards/dto/create-board.dto.ts
@@ -1,5 +1,9 @@
+import { IsNotEmpty } from "class-validator"
+
 export class CreateBoardDto{
+  @ IsNotEmpty() // 유효성 검사, 비어있지 않게끔
   title:string
+  @IsNotEmpty() // 유효성 검사, 비어있지 않게끔
   description:string
 
 }
