f8bb379510f7cf9c09134527611fba7e652e111f
k0789789 <k0789789@naver.com>
2023-10-25 22:58:22 +0900

    Refactor 모듈 내 컨트롤러 서비스 명시

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,16 @@ import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './config/typeorm.config';
+import { BoardRepository } from './boards/board.repository';
+import { BoardsController } from './boards/boards.controller';
+import { BoardsService } from './boards/boards.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(typeORMConfig),
-    BoardsModule
+    TypeOrmModule.forFeature([BoardRepository]),  
   ],
+  controllers : [BoardsController],
+  providers : [BoardsService]
+
 })
 export class AppModule {}
