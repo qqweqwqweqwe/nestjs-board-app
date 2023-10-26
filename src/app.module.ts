b2fba2aff703d20d981a2ecc2af06d5b4655db92
k0789789 <k0789789@naver.com>
2023-10-26 20:31:07 +0900

    Create auth 모듈

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { typeORMConfig } from './config/typeorm.config';
 import { BoardRepository } from './boards/board.repository';
 import { BoardsController } from './boards/boards.controller';
 import { BoardsService } from './boards/boards.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([BoardRepository]),  
+    TypeOrmModule.forFeature([BoardRepository]),
+    AuthModule,  
   ],
   controllers : [BoardsController],
   providers : [BoardsService]
