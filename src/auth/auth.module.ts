4c00f4796b5dc177536a69af769f337548818c8d
k0789789 <k0789789@naver.com>
2023-10-26 23:56:00 +0900

    Feat jwt 모듈 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,12 +3,23 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports:[
-    TypeOrmModule.forFeature([UserRepository]) //  유저 리포지토리를 다른곳에서도
+    PassportModule.register({defaultStrategy : 'jwt'}),
+    JwtModule.register({
+      secret:'Secret1234', // 더해주는 시크릿 텍스트
+      signOptions:{
+        expiresIn : 60*60 // 토큰을 어느정도 유효하게 해줄건지 (1시간)
+      }
+    }),
+    TypeOrmModule.forFeature([UserRepository]) //  유저 리포지토리를 다른곳에서도 사용할 수 있게끔
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService,JwtStrategy],
+  exports:[JwtStrategy,PassportModule] // 다른 모듈에서 사용해주기 위해서 넣어주는것 
 })
 export class AuthModule {}
