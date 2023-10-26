260269fec067bdafc71acc71676d541e2fb407c9
k0789789 <k0789789@naver.com>
2023-10-26 11:13:39 +0900

    Refactor Repository 기반 코드 리팩토

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,52 +1,53 @@
-import { Body, Controller,Get, Post,Param, Delete, Patch, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller,Get, Param } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { Board, BoardStatus } from './board.model';
-import { CreateBoardDto } from './dto/create-board.dto';
-import { BoardStatusvalidationPipe } from './pipes/board-status-validation.pipes';
+import { Board } from './board.entity';
 
-@Controller('boards')
+@Controller('boards') 
 export class BoardsController {
-  // boardService : BoardsService
+  constructor (private boardService : BoardsService){}
   
-  // constructor(boardService : BoardsService){
-  //   this.boardService=boardService;
+  // // constructor(boardService : BoardsService){
+  // //   this.boardService=boardService;
+  // // }
+  // // 위 방식처럼 해도 되지만 이렇게 해도됨, 보드서비스 주입
+  // constructor(private boardService : BoardsService){}
+  // @Get()
+  // getAllBoard() : Board[]{
+  // return this.boardService.getAllboards()
   // }
-  // 위 방식처럼 해도 되지만 이렇게 해도됨, 보드서비스 주입
-  constructor(private boardService : BoardsService){}
-  @Get()
-  getAllBoard() : Board[]{
-  return this.boardService.getAllboards()
-  }
 
-  @Post()
-  @UsePipes(ValidationPipe) // 유효성 검사 파이프 추가 (데코레이션 레벨)
-  creaateBoard(
-    @Body() createBoardDto : CreateBoardDto
-    )
-    :Board      {
-    return this.boardService.createBoard(createBoardDto)
-  }
-
+  // @Post()
+  // @UsePipes(ValidationPipe) // 유효성 검사 파이프 추가 (데코레이션 레벨)
+  // creaateBoard(
+  //   @Body() createBoardDto : CreateBoardDto
+  //   )
+  //   :Board      {
+  //   return this.boardService.createBoard(createBoardDto)
+  // }
 
   @Get("/:id")
-  // 위에 "/:id"니까 id로 가져오면 됨
-  getBoardById(@Param('id') id :string){
+  getBoardById(@Param('id') id :number) :Promise<Board>{
     return this.boardService.getBoardById(id)
   }
+  // @Get("/:id")
+  // // 위에 "/:id"니까 id로 가져오면 됨
+  // getBoardById(@Param('id') id :string){
+  //   return this.boardService.getBoardById(id)
+  // }
 
-  @Delete('/:id')
-  deleteBoard(@Param('id') id :string){
-    this.boardService.deleteBoard(id)
-  }
+  // @Delete('/:id')
+  // deleteBoard(@Param('id') id :string){
+  //   this.boardService.deleteBoard(id)
+  // }
 
-  // param과 body의 차이는 param은 주소에 포함된 변수를 담고, body는 json등에 포함된 변수를 담는다
-  @Patch('/:id/status')
-  updateBoardStatus(
-    @Param('id') id:string,
-    @Body('status', BoardStatusvalidationPipe) status:BoardStatus  // status만 유효성 검사 해주는 것임
+  // // param과 body의 차이는 param은 주소에 포함된 변수를 담고, body는 json등에 포함된 변수를 담는다
+  // @Patch('/:id/status')
+  // updateBoardStatus(
+  //   @Param('id') id:string,
+  //   @Body('status', BoardStatusvalidationPipe) status:BoardStatus  // status만 유효성 검사 해주는 것임
 
-  ){
-    return this.boardService.updateBoardStatus(id,status)
-  }
+  // ){
+  //   return this.boardService.updateBoardStatus(id,status)
+  // }
   
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,65 +1,83 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
 import {v1 as uuid} from 'uuid' // uuid의 v1버전을 uuid라는 이름으로 사용하게끔
 import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardRepository } from './board.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Board } from './board.entity';
 
 @Injectable()  // 이 데코레이션을 사용함으로써 다른곳에서 보드서비스 접근 가능, 즉 어플리케이션 전체에서 사용 가능 
 export class BoardsService {
 
-  private boards: Board[]=[] 
-  // private으로 하는 이유는 다른 곳에서 접근하지 못하게 하기 위해서 
-  // 뒤에 Bord[] 가 의미하는 것은 저 배열이 Board배열임을 명시해주기 위함임
+  constructor(
+    // 이제부터 서비스 안에서 board repository 사용 가능
+    @InjectRepository(BoardRepository)
+    private boardRepository : BoardRepository,
+
+  ){}
+  // private boards: Board[]=[] 
+  // // private으로 하는 이유는 다른 곳에서 접근하지 못하게 하기 위해서 
+  // // 뒤에 Bord[] 가 의미하는 것은 저 배열이 Board배열임을 명시해주기 위함임
   
 
-  // 모든 게시물들 가져오는 함수
-  getAllboards() : Board[] { // 여기의 Board[]는 return 값이 Board[]라는 의미임
-    return this.boards;
-  }
+  // // 모든 게시물들 가져오는 함수
+  // getAllboards() : Board[] { // 여기의 Board[]는 return 값이 Board[]라는 의미임
+  //   return this.boards;
+  // }
 
-  createBoard(createBoardDto : CreateBoardDto){
-    // const title  = createBoardDto.title
-    // const description = createBoardDto.description
+  // createBoard(createBoardDto : CreateBoardDto){
+  //   // const title  = createBoardDto.title
+  //   // const description = createBoardDto.description
 
-    //근데 위랑 아래랑 같다 아래가 더 편하니까 아래처럼 ㄱ
-    const {title,description} = createBoardDto
-      const board={
-        id : uuid(),  // 이제는 유니크한 값을 아이디로 줄 수 있음
-        title :title,
-        description : description,
-        status:BoardStatus.PUBLIC
-      }
-    this.boards.push(board) // 새로운 게시물을 넣어줌
-    return board;
-  }
+  //   //근데 위랑 아래랑 같다 아래가 더 편하니까 아래처럼 ㄱ
+  //   const {title,description} = createBoardDto
+  //     const board={
+  //       id : uuid(),  // 이제는 유니크한 값을 아이디로 줄 수 있음
+  //       title :title,
+  //       description : description,
+  //       status:BoardStatus.PUBLIC
+  //     }
+  //   this.boards.push(board) // 새로운 게시물을 넣어줌
+  //   return board;
+  // }
  
-
-  // 서비스를 먼저 구현한 후 컨트롤러로 옮기는것
-  
-  getBoardById(id :string):Board{
-    // boards안에서 보드의 id와 매개변수 id가 같은 보드를 찾는것 
-    const found =this.boards.find((board) => board.id===id); 
-
-
-    if (!found){ // 만약에 없다면 예외 throw
-      throw new NotFoundException(`Can't find board witg id ${id} `); // 오류 메세지 출력
+  async getBoardById(id:number):Promise <Board>{
+    const found = await this.boardRepository.findOne(id)
+    
+    if(!found){
+      throw new NotFoundException(`can't find Board With id ${id}`)
     }
 
-    return found 
+    return found
 
-  }
+  } 
 
-  deleteBoard(id: string):void{
-    // id가 다른 게시물만 남겨준다는 의미임
+  // // 서비스를 먼저 구현한 후 컨트롤러로 옮기는것
+  
+  // getBoardById(id :string):Board{
+  //   // boards안에서 보드의 id와 매개변수 id가 같은 보드를 찾는것 
+  //   const found =this.boards.find((board) => board.id===id); 
 
-    // 이렇게 해주면 없을때 알아서 에러 던졎ㅁ
-    const found = this.getBoardById(id)
 
-    this.boards = this.boards.filter((board)=> board.id!==found.id)
-  }
+  //   if (!found){ // 만약에 없다면 예외 throw
+  //     throw new NotFoundException(`Can't find board witg id ${id} `); // 오류 메세지 출력
+  //   }
 
-  updateBoardStatus(id :string, status:BoardStatus):Board{
-    const board = this.getBoardById(id)
-    board.status = status;
-    return board
-  }
+  //   return found 
+
+  // }
+
+  // deleteBoard(id: string):void{
+  //   // id가 다른 게시물만 남겨준다는 의미임
+
+  //   // 이렇게 해주면 없을때 알아서 에러 던졎ㅁ
+  //   const found = this.getBoardById(id)
+
+  //   this.boards = this.boards.filter((board)=> board.id!==found.id)
+  // }
+
+  // updateBoardStatus(id :string, status:BoardStatus):Board{
+  //   const board = this.getBoardById(id)
+  //   board.status = status;
+  //   return board
+  // }
 }
