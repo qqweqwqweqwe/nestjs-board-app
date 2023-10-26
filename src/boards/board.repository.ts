a2d4fc5745c6261d3721126c6bca5d5d4200e454
k0789789 <k0789789@naver.com>
2023-10-26 18:00:40 +0900

    Feat 게시글 생성 함수 구현

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -1,13 +1,25 @@
 import { EntityRepository, Repository } from "typeorm";
 import { Board } from "./board.entity";
 import { Injectable } from "@nestjs/common";
+import { CreateBoardDto } from "./dto/create-board.dto";
+import { BoardStatus } from "./board.status.enum";
 
 
-@Injectable()
 @EntityRepository(Board)
 export class BoardRepository extends Repository<Board>{
   // @EntityRepository 가 deprecated 됐기 때문에 이렇게 해주어야함
   // 이것이 board를 위한 repository임을 알려주는 코드}
+  async createBoard(createBoardDto : CreateBoardDto):Promise<Board>{
+    const {title, description}= createBoardDto;
+  const board =this.create({
+    title,
+    description,
+    status : BoardStatus.PUBLIC
+  })
 
+  await this.save(board)
+  return board
+  }
+  
 
 }
