a5137df1bf33ba4812e2b2796f192be82a96dd8d
k0789789 <k0789789@naver.com>
2023-10-26 11:14:04 +0900

    Fefactor 리포지토리 기반 코드 리팩토

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -1,15 +1,13 @@
-import { DataSource, EntityRepository, Repository } from "typeorm";
+import { EntityRepository, Repository } from "typeorm";
 import { Board } from "./board.entity";
 import { Injectable } from "@nestjs/common";
 
 
 @Injectable()
+@EntityRepository(Board)
 export class BoardRepository extends Repository<Board>{
   // @EntityRepository 가 deprecated 됐기 때문에 이렇게 해주어야함
-  // 이것이 board를 위한 repository임을 알려주는 코드
-  constructor(dataSource: DataSource) {
-    super(Board, dataSource.createEntityManager());
-}
+  // 이것이 board를 위한 repository임을 알려주는 코드}
 
 
 }
