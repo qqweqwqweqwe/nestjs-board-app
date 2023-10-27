3b4cb9fc62159d02a53fde423b57e6ba883eb01f
k0789789 <k0789789@naver.com>
2023-10-27 15:08:02 +0900

    Feat 유저와 게시글 사이 i:n관게 추가

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,7 +1,10 @@
 import { IsString, Matches, MaxLength, MinLength, minLength } from "class-validator";
-import { BaseEntity, Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { type } from "os";
+import { Board } from "src/boards/board.entity";
+import { BaseEntity, Column, Entity, OneToMany, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
+@unique(['username']) // username이  unique해질수 있도록 하는 대코레이션
 export class User extends BaseEntity{
   @PrimaryGeneratedColumn()
   id:number;
@@ -23,4 +26,12 @@ export class User extends BaseEntity{
   )
   @Column()
   password : string;
+
+  // 유저와 게시글의 관계는 1:n관계, 하나의 유저는 게시글이 여러개므로
+  @OneToMany(type=>Board, board=>board.user,{eager : true}) // eager true는 유저를 가져올때 게시물도 같이 가져오게끔
+  boards : Board[]
+}
+
+function unique(arg0: string[]): (target: typeof User) => void | typeof User {
+  throw new Error("Function not implemented.");
 }
