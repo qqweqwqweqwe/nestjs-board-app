688de06abce9c44f6f6005fe5c78335c48cb7178
k0789789 <k0789789@naver.com>
2023-10-27 15:11:41 +0900

    Refactor 쿼리문형식 리팩토링

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -5,6 +5,8 @@ import { BoardRepository } from './board.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Board } from './board.entity';
 import { BoardStatus } from './board.status.enum';
+import { User } from 'src/auth/user.entity';
+import { use } from 'passport';
 
 @Injectable()  // 이 데코레이션을 사용함으로써 다른곳에서 보드서비스 접근 가능, 즉 어플리케이션 전체에서 사용 가능 
 export class BoardsService {
@@ -20,16 +22,25 @@ export class BoardsService {
   // // 뒤에 Bord[] 가 의미하는 것은 저 배열이 Board배열임을 명시해주기 위함임
   
 
-  async getAllBoards() : Promise<Board[]>{
-    return this.boardRepository.find() // 모든 게시물 가져오는 것
+  async getAllBoards(
+    user:User
+  ) : Promise<Board[]>{
+    const query = this.boardRepository.createQueryBuilder('board'); // 보드 테이블에 대해서 작성할 쿼리 생성
+
+    query.where('board.userId = :userId', {userId:user.id}) // 게시글작성자가 유저 아이디랑 같은지
+
+    const boards =await query.getMany() // getmany는 다 가져오는거
+
+
+    return boards
   }
 
   // // 모든 게시물들 가져오는 함수
   // getAllboards() : Board[] { // 여기의 Board[]는 return 값이 Board[]라는 의미임
   //   return this.boards;
   // }
-async createBoard(createBoardDto : CreateBoardDto): Promise<Board>{  // async의 return값은 프로미즈(복습한내용)
-  return this.boardRepository.createBoard(createBoardDto)
+async createBoard(createBoardDto : CreateBoardDto, user:User): Promise<Board>{  // async의 return값은 프로미즈(복습한내용)
+  return this.boardRepository.createBoard(createBoardDto,user)
 }
   // createBoard(createBoardDto : CreateBoardDto){
   //   // const title  = createBoardDto.title
@@ -73,8 +84,8 @@ async createBoard(createBoardDto : CreateBoardDto): Promise<Board>{  // async의
 
   // }
 
-  async deleteBoard(id :number ) :Promise<void>{
-    const result =await this.boardRepository.delete(id) //  db에서 id값의 게시물을 삭제해줌, 없으면 아무일도 일어나지 않음
+  async deleteBoard(id :number, user:User ) :Promise<void>{
+    const result =await this.boardRepository.delete({id,user}) //  db에서 id값의 게시물을 삭제해줌, 없으면 아무일도 일어나지 않음
     
     if (result.affected===0){ // 영향을 준것이 0이라는 것은 해당 게시글이 없다는 소리, 즉 에러 throw
       throw new NotFoundException(`can't find board with id ${id}`)
