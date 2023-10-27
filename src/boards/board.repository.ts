92a085a2955d3876c9d5e838e3cc79bb376e92e1
k0789789 <k0789789@naver.com>
2023-10-27 15:09:05 +0900

    Feat CreateBoard 유저 매개변수 추가

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -3,18 +3,20 @@ import { Board } from "./board.entity";
 import { Injectable } from "@nestjs/common";
 import { CreateBoardDto } from "./dto/create-board.dto";
 import { BoardStatus } from "./board.status.enum";
+import { User } from "src/auth/user.entity";
 
 
 @EntityRepository(Board)
 export class BoardRepository extends Repository<Board>{
   // @EntityRepository 가 deprecated 됐기 때문에 이렇게 해주어야함
   // 이것이 board를 위한 repository임을 알려주는 코드}
-  async createBoard(createBoardDto : CreateBoardDto):Promise<Board>{
+  async createBoard(createBoardDto : CreateBoardDto, user:User):Promise<Board>{
     const {title, description}= createBoardDto;
   const board =this.create({
     title,
     description,
-    status : BoardStatus.PUBLIC
+    status : BoardStatus.PUBLIC,
+    user
   })
 
   await this.save(board)
